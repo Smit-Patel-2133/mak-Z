f3386eede638b164bf6475080ebedac9bb15a642
smit patel 2133 <smitnpatel013@gmail.com>
2024-03-07 22:35:44 +0530

    Login and SignUp page Complite in FromtEnd

--- Front End/mak Z/src/App.jsx
+++ Front End/mak Z/src/App.jsx
@@ -1,17 +1,30 @@
-
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Login from "./components/login/Login.jsx";
+import SignUp from "./components/SingUp/SignUp.jsx";
 
 function App() {
-
-
-  return (
-    <>
+    return (
         <div>
-            <Login/>
+            <Router>
+                <Routes>
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/signup" element={<SignUp/>}/>
+                </Routes>
 
+                <button
+                    className="flex justify-center items-center mt-36 ml-36 px-6 py-2.5 font-medium text-white bg-blue-500 rounded-md focus:outline-none hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                    tabIndex="0"
+                    onClick={() => window.location.href = '/login'}>Login
+                </button>
+                <button
+                    className="flex justify-center items-center mt-36 ml-36 px-6 py-2.5 font-medium text-white bg-blue-500 rounded-md focus:outline-none hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                    tabIndex="0"
+                    onClick={() => window.location.href = '/signup'}>Sign Up
+                </button>
+            </Router>
         </div>
-    </>
-  )
+    );
 }
 
-export default App
+export default App;
--- Front End/mak Z/src/components/login/Login.jsx
+++ Front End/mak Z/src/components/login/Login.jsx
@@ -1,11 +1,12 @@
-import React, {useState} from 'react';
-import WLogo from '../../assets/picture/white_logo.png'
+import React, {useState} from "react";
+import WLogo from "../../assets/picture/white_noBG.png";
 
 const Login = (props) => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const handleUsernameChange = (event) => {
-        setUsername(event.target.value);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
     };
 
     const handlePasswordChange = (event) => {
@@ -14,48 +15,78 @@ const Login = (props) => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        // You can add your login logic here, such as sending a request to the server
-        console.log('Username:', username);
-        console.log('Password:', password);
+        // Your login logic here (e.g., API request)
+        console.log("Email:", email);
+        console.log("Password:", password);
     };
+
     return (
-        <>
-            <img loading="lazy" src={WLogo} alt="logo"
-                 className="max-w-full w-[100px] mt-10 ml-10 rounded-full"/>
-            <div className="flex justify-center items-center mt-20 px-16 py-12 text-sm leading-5 bg-white sm:px-5">
-                <form
-                    className="flex flex-col justify-center items-center px-20 py-8 bg-gray-100 w-[500px] sm:px-5 sm:py-4">
-                    <h2 className="mt-8 text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
-                    <div className="mt-8 w-full">
-                        <label htmlFor="email" className="sr-only">Email address</label>
-                        <input type="email" id="email" name="email" placeholder="Email address"
-                               aria-label="Email address"
-                               className="justify-center px-3 py-2.5 w-full text-gray-900 bg-white rounded-md border border-gray-300"/>
+        <div className="flex flex-col sm:flex-row">
+            {/* Black Part (Logo) */}
+            <div className="w-full sm:w-1/2 h-screen bg-gray-800 flex flex-col items-center justify-center">
+                <img
+                    loading="lazy"
+                    src={WLogo}
+                    alt="logo"
+                    className="max-w-full w-[600px] rounded-full mb-4 ml-4 mt-4"
+                />
+            </div>
+
+            {/* White Part (Login Form) */}
+            <div className="bg-gray-100 w-full sm:w-1/2 p-4 sm:p-8 h-screen flex items-center justify-center">
+                <form className="flex flex-col items-center max-w-md w-full bg-white rounded-md shadow-md p-6">
+                    <h2 className="text-3xl font-extrabold text-gray-800 mb-8">
+                        Sign in to your account
+                    </h2>
+                    <div className="w-full mb-4">
+                        <label htmlFor="email" className="sr-only">
+                            Email address
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Email address"
+                            aria-label="Email address"
+                            className="w-full px-3 py-2.5 text-gray-800 bg-white rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                            onChange={handleEmailChange}
+                        />
                     </div>
-                    <div className="mt-4 w-full">
-                        <label htmlFor="password" className="sr-only">Password</label>
-                        <input type="password" id="password" name="password" placeholder="Password"
-                               aria-label="Password"
-                               className="justify-center px-3 py-2.5 w-full text-gray-900 bg-white rounded-md border border-gray-300"/>
+                    <div className="w-full mb-4">
+                        <label htmlFor="password" className="sr-only">
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Password"
+                            aria-label="Password"
+                            className="w-full px-3 py-2.5 text-gray-800 bg-white rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                            onChange={handlePasswordChange}
+                        />
                     </div>
-                    <div className="flex gap-5 justify-between mt-6 w-full">
-                        <div className="flex items-center">
-                            <input type="checkbox" id="remember-me" name="remember-me" className="mr-2"/>
-                            <label htmlFor="remember-me" className="text-gray-900">Remember me</label>
-                        </div>
-                        <button className="font-medium text-rose-500 focus:outline-none focus:underline"
-                                tabIndex="0">Forgot your password?
+                    <div className="flex justify-between items-center w-full mb-4">
+                        <div className="flex items-center"></div>
+                        <button
+                            className="font-medium text-gray-600 focus:outline-none hover:underline"
+                            tabIndex="0"
+                        >
+                            Forgot your password?
                         </button>
                     </div>
-                    <button type="submit"
-                            className="flex justify-center items-center px-16 py-2.5 mt-6 w-full font-medium text-white bg-rose-500 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500"
-                            tabIndex="0">Sign in
+                    <button
+                        type="submit"
+                        className="flex justify-center items-center w-full px-6 py-2.5 font-medium text-white bg-blue-500 rounded-md focus:outline-none hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                        tabIndex="0"
+                        onClick={handleSubmit}
+                    >
+                        Sign in
                     </button>
                 </form>
             </div>
-        </>
+        </div>
     );
 };
-
 
 export default Login;
