be3fcd50f5b2e373d79f4170f5ed4e311f339f62
smit patel 2133 <smitnpatel013@gmail.com>
2024-03-07 22:42:11 +0530

    Login and SignUp page Complite in FromtEnd

--- Front End/mak Z/src/App.jsx
+++ Front End/mak Z/src/App.jsx
@@ -13,12 +13,12 @@ function App() {
                 </Routes>
 
                 <button
-                    className="flex justify-center items-center mt-36 ml-36 px-6 py-2.5 font-medium text-white bg-blue-500 rounded-md focus:outline-none hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                    className="flex justify-center items-center mt-35 ml-36 px-6 py-2.5 font-medium text-white bg-blue-500 rounded-md focus:outline-none hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                     tabIndex="0"
                     onClick={() => window.location.href = '/login'}>Login
                 </button>
                 <button
-                    className="flex justify-center items-center mt-36 ml-36 px-6 py-2.5 font-medium text-white bg-blue-500 rounded-md focus:outline-none hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                    className="flex justify-center items-center mt-35 ml-36 px-6 py-2.5 font-medium text-white bg-blue-500 rounded-md focus:outline-none hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                     tabIndex="0"
                     onClick={() => window.location.href = '/signup'}>Sign Up
                 </button>
