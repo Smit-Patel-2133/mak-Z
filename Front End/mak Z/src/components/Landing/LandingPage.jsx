d5925d95e3b76dc860a9f8ac3e6638762063daf4
Adarsh S Lodhi <adarshhloghi@gmail.com>
2024-04-04 15:01:29 +0530

    update in the Landing page design

--- Front End/mak Z/src/components/Landing/LandingPage.jsx
+++ Front End/mak Z/src/components/Landing/LandingPage.jsx
@@ -3,11 +3,12 @@ import { useNavigate } from 'react-router-dom';
 import WLNB from '../../assets/picture/new White NO BG.png';
 import l1 from "../../assets/landin images/landing image 1.png";
 
-
+import './Landing.css'
 const LandingPage = () => {
     const navigate = useNavigate();
 
     return (
+        <>
         <div className="min-h-screen bg-black flex flex-col items-center justify-center text-white relative">
             <div className="absolute top-10 animate-fade-in ">
                 <img src={WLNB} className="h-10 sm:h-20 md:h-20 lg:h-20 xl:h-20" alt="Logo" />
@@ -29,6 +30,26 @@ const LandingPage = () => {
                 <img src={l1} alt="Landing Image" className='h-40 sm:h-48 md:h-56 lg:h-64 xl:h-72' />
             </div>
         </div>
+        <div className='pt-5'>
+            <div className='row'>
+        <div class="extra-div col-lg-4 col-md-12">
+          <a href="#"><i class="fa-3x fa fa-desktop" aria-hidden="true"></i></a>
+          <h2>EASY TO USE</h2>
+          <p>We’re launching another issue of RocketSTEM to celebrate the start of our organization’s fifth year. While half a decade has been quite a journey for us, it is nothing compared to the 50-plus-year history of Kennedy Space Center’s Launch Complex 39A.</p>
+        </div>
+        <div class="extra-div col-lg-4 col-md-12">
+          <a href="#"><i class="fa-3x fa fa-trophy" aria-hidden="true"></i></a>
+          <h2>AWESOM DESIGN</h2>
+          <p>We’re launching another issue of RocketSTEM to celebrate the start of our organization’s fifth year. While half a decade has been quite a journey for us, it is nothing compared to the 50-plus-year history of Kennedy Space Center’s Launch Complex 39A.</p>
+        </div>
+        <div class="extra-div col-lg-4 col-md-12">
+          <a href="#"><i class="fa-3x fa fa-magic" aria-hidden="true"></i></a>
+          <h2>EASY TO CUSTOMIZE</h2>
+          <p>We’re launching another issue of RocketSTEM to celebrate the start of our organization’s fifth year. While half a decade has been quite a journey for us, it is nothing compared to the 50-plus-year history of Kennedy Space Center’s Launch Complex 39A.</p>
+        </div>
+        </div>
+        </div>
+        </>
     );
 }
 
