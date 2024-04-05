13c0ba33c55b77e4e6934e4bb2092eaf829147b6
AbhyaHingrajiya <abhayhingrajiya18@gmail.com>
2024-04-05 23:29:21 +0530

    Download file function setup done

--- Back End/server.js
+++ Back End/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 const bodyParser = require('body-parser');
 const sql = require('./db');
 
 const app = express();
-
 app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
@@ -49,6 +61,50 @@ app.post('/login', async (req, res) => {
         res.status(500).json({ error: 'Internal server error', details: error.message });
     }
 });
+
+app.post('/download', async (req, res) => {
+    const codeData = req.body.code;
+    const filePath = path.join(__dirname, '../../mak-Z.html');
+
+    fs.appendFile(filePath, codeData, { encoding: 'utf8', flag: 'a+' }, (err) => {
+        if (err) {
+            console.error('Error appending data to file:', err);
+            return res.status(500).send('Internal Server Error');
+        }
+
+        console.log('Data appended to file successfully.');
+
+        const options = {
+            headers: {
+                'Content-Type': 'text/html'
+            }
+        };
+
+        res.set('Content-Type', 'text/html');
+
+        res.sendFile(filePath, options, (err) => {
+            if (err) {
+                console.error('Error downloading file:', err);
+                return res.status(500).send('Internal Server Error');
+            }
+        });
+    });
+});
+
+app.delete('/delete/:filename', (req, res) => {
+    const filename = req.params.filename;
+    const filePath = path.join(__dirname, '../../mak-Z.html');
+    fs.unlink(filePath, (err) => {
+        if (err) {
+            console.error('Error deleting file:', err);
+            res.status(500).send('Error deleting file');
+        } else {
+            console.log('File deleted successfully:', filename);
+            res.status(200).send('File deleted successfully');
+        }
+    });
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
