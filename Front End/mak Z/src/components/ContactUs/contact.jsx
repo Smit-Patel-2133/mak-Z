eae3486ced4ed0114cd88f675e42a4d79c4c81d5
Adarsh S Lodhi <adarshhloghi@gmail.com>
2024-04-22 20:51:34 +0530

    ContactUs Page

---
+++ Front End/mak Z/src/components/ContactUs/contact.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+//import './header.css';
+
+const Contact = () => {
+  return (
+    <div style={{ display: 'flex', width: '100vw', height: '100vh', background: '#FCFDFD' }}>
+      <div style={{ width: '45vw', height: '100%', backgroundImage: 'url("../src/assets/picture/contactus.jpg")', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
+        
+      </div>
+      <div style={{ background: '#FCFDFD', width: '50vw', height: '100vh', padding: '5rem 3.5rem' }}>
+        <div style={{ display: 'flex', alignItems: 'center', textAlign: 'center', width: '160px', position: 'absolute', top: '0.5rem', right: 0 }}>
+          
+        </div>
+
+        <h1 style={{ color: '#3F444C', textTransform: 'uppercase', fontSize: '2.5rem', letterSpacing: '0.5rem', fontWeight: 300 }}>Contact us</h1>
+        <p style={{ color: '#818386', fontSize: '0.9rem', letterSpacing: '0.01rem', width: '40vw', margin: '0.25rem 0' }}>Code with Ease, Design with Style - Mak: Your Drag-and-Drop Website Wizard !</p>
+
+        <form id="contact-form" method="post" style={{ width: '31.25rem', position: 'relative', marginTop: '2rem', padding: '1rem 0' }}>
+          <label htmlFor="name" style={{ color: '#818386', textTransform: 'uppercase', fontSize: '0.625rem' }}>Full name</label>
+          <input type="text" id="name" name="name" placeholder="Your Full Name" required style={{ color: '#010712', fontWeight: 500, background: '#FCFDFD', border: 'none', borderBottom: '1px solid #818386', padding: '0.5rem 0', marginBottom: '1rem', outline: 'none', width: '40vw' }} />
+          <label htmlFor="email" style={{ color: '#818386', textTransform: 'uppercase', fontSize: '0.625rem' }}>Email Address</label>
+          <input type="email" id="email" name="email" placeholder="Your Email Address" required style={{ color: '#010712', fontWeight: 500, background: '#FCFDFD', border: 'none', borderBottom: '1px solid #818386', padding: '0.5rem 0', marginBottom: '1rem', outline: 'none', width: '40vw' }} />
+          <label htmlFor="message" style={{ color: '#818386', textTransform: 'uppercase', fontSize: '0.625rem' }}>Message</label>
+          <textarea rows="6" placeholder="Your Message" id="message" name="message" required style={{ color: '#010712', fontWeight: 500, background: '#FCFDFD', border: 'none', borderBottom: '1px solid #818386', padding: '0.5rem 0', marginBottom: '1rem', outline: 'none', width: '40vw', resize: 'none' }}></textarea>
+          <button type="submit" id="submit" name="submit" style={{ textTransform: 'uppercase', fontWeight: 300, background: '#3B3636', color: '#FCFDFD', width: '10rem', height: '2.25rem', border: 'none', borderRadius: '2px', outline: 'none', cursor: 'pointer' }}>Send</button>
+        </form>
+        <div id="error" style={{ width: '40vw', margin: '0.125rem 0', fontSize: '0.75rem', textTransform: 'uppercase', fontFamily: 'Jost', color: '#818386' }}></div>
+        <div id="success-msg" style={{ width: '40vw', margin: '0.125rem 0', fontSize: '0.75rem', textTransform: 'uppercase', fontFamily: 'Jost', color: '#818386', transitionDelay: '3s' }}></div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- Front End/mak Z/src/components/Header/Header.jsx
+++ Front End/mak Z/src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 // Header.js
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom'; // Import NavLink
+import { NavLink, useNavigate } from 'react-router-dom'; // Import NavLink
 import logo from '../../assets/picture/new black noBG.png';
 import './header.css';
 
 const Header = () => {
     const user = useSelector(state => state.auth);
+    const navigate =useNavigate()
 
     return (
         <div className='bg-blue-700'>
@@ -18,7 +19,7 @@ const Header = () => {
                 ) : (
                     <li><NavLink to="/login">Login</NavLink></li>
                     )}
-                <li><NavLink to="/">Contact Us</NavLink></li> {/* Example NavLink */}
+                <li><NavLink to="/ContactUs">Contact Us</NavLink></li> {/* Example NavLink */}
                 <li><NavLink to="/">About Us</NavLink></li> {/* Example NavLink */}
                 <li><NavLink to="/templates">Template</NavLink></li> {/* Example NavLink */}
                 <li><NavLink to="/home">Home</NavLink></li> {/* Example NavLink */}
