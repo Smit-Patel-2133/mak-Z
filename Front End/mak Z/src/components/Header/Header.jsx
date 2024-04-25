bec3f2c43044b964fac4bdd85c65ac5a68543b6e
DhruvBhatt13 <bhattdhruv996@gmail.com>
2024-04-25 17:26:33 +0530

    just for pull request

--- Front End/mak Z/src/components/Header/Header.jsx
+++ Front End/mak Z/src/components/Header/Header.jsx
@@ -1,13 +1,8 @@
 // Header.js
 import React from 'react';
 import { useSelector } from 'react-redux';
-<<<<<<< HEAD
-import { NavLink } from 'react-router-dom'; // Import NavLink
-import logo from '../../assets/picture/new white cap noBG.png';
-=======
 import { NavLink, useNavigate } from 'react-router-dom'; // Import NavLink
 import logo from '../../assets/picture/new black noBG.png';
->>>>>>> eae3486ced4ed0114cd88f675e42a4d79c4c81d5
 import './header.css';
 
 const Header = () => {
@@ -24,13 +19,8 @@ const Header = () => {
                 ) : (
                     <li><NavLink to="/login">Login</NavLink></li>
                     )}
-<<<<<<< HEAD
-                <li><NavLink to="/">Contact Us</NavLink></li> {/* Example NavLink */}
                 <li><NavLink to="/AboutUs">About Us</NavLink></li> {/* Example NavLink */}
-=======
                 <li><NavLink to="/ContactUs">Contact Us</NavLink></li> {/* Example NavLink */}
-                <li><NavLink to="/">About Us</NavLink></li> {/* Example NavLink */}
->>>>>>> eae3486ced4ed0114cd88f675e42a4d79c4c81d5
                 <li><NavLink to="/templates">Template</NavLink></li> {/* Example NavLink */}
                 <li><NavLink to="/home">Home</NavLink></li> {/* Example NavLink */}
             </ul>
