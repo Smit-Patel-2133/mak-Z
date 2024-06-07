71e73c20ad2e92d561bdd5f74c50312f31a069ea
smit patel 2133 <smitnpatel013@gmail.com>
2024-06-07 21:01:17 +0530

    abhay preview

--- Front End/mak Z/src/components/Admin/TemplatePreviewModal.jsx
+++ Front End/mak Z/src/components/Admin/TemplatePreviewModal.jsx
@@ -5,21 +5,7 @@ const TemplatePreviewModal = ({ isOpen, onRequestClose, htmlContent }) => {
     console.log("this is the html content:=", htmlContent);
     return (
         <div>
-            {isOpen && (
-                <div className="modal-overlay">
-                    <button className="absolute top-2 right-2 text-xl h-25 w-20 bg-red-950" onClick={onRequestClose}>×
-                    </button>
 
-                    <div className="modal">
-                        {/* Display the base64 image content */}
-                        <div className="modal-content">
-                            <img src={`data:image/jpeg;base64,${htmlContent}`} alt="Template Preview" />
-                        </div>
-                        {/* Additional close button */}
-                        <button className="absolute bottom-2 right-2 text-xl" onClick={onRequestClose}>Close</button>
-                    </div>
-                </div>
-            )}
         </div>
     );
 };
